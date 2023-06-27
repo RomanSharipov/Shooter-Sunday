5c334353a2f029bd3924c35fe6d1cc6c3c408dd5
RomanSharipov <roman.sharipov.1990@gmail.com>
2023-06-27 17:19:52 +0500

    remoe needless sctipts

--- Assets/Scripts/Movement/RigidbodyMovement.cs
+++ Assets/Scripts/Movement/RigidbodyMovement.cs
@@ -7,16 +7,12 @@ public class RigidbodyMovement : MonoBehaviour
     [SerializeField] private Rigidbody _rigidbody;
     [SerializeField] private InputMain _inputMain;
     [SerializeField] private float _moveSpeed = 3.0f;
+
     private Vector3 _direction = new Vector3();
 
     private void Update()
     {
-        //Vector3 localMovementDirection = new Vector3(_inputMain.MovementDirection.x, 0, _inputMain.MovementDirection.y);
-        //Vector3 worldMovementDirection = transform.TransformDirection(localMovementDirection);
-        //_characterController.Move(worldMovementDirection * _moveSpeed * Time.deltaTime);
-        //ApplyGravity();
         Walk(_inputMain.MovementDirection);
-
     }
 
     private void Walk(Vector2 direction)
